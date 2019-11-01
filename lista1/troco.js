34eba4639cb50bc15fd4e836cefea34a407ebd8c
lucas.pimenta01@etec.sp.gov.br <lucas.pimenta01@etec.sp.gov.br>
2019-11-01 20:42:24 -0300

    Initial commit

---
+++ lista1/troco.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <script language = Javascript>
+    valor1=("valor por kg");
+    valor2=("kg comprados");
+valor=parseFloat(valor1)*parseFloat(valo2)
+alert("valor"=troco);
+
+
+        </script>
+        <body></body>
+    </head>
+    </html>
---
+++ lista1/troco1.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <script language = javascript>
+    valor1=prompt("valor do produto");
+    valor2=prompt("valor pago");
+troco= parseFloat(valor1) - parseFloat(valor2);
+alert("troco="+troco);
+
+
+        </script>
+        <body></body>
+    </head>
+    </html>
